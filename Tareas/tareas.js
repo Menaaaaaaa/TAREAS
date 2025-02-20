a81a0316f78b77bda9c6c5c50c59f6eb19f33f28
Camilo Villegas <cam.aristi12@gmail.com>
2025-02-20 01:17:04 -0500

    se utiliza localStorage y se añade crear y listar tareas

--- Tareas/tareas.js
+++ Tareas/tareas.js
@@ -1,8 +1,67 @@
-const input = document.getElementById('ingresarTarea');
-const boton = document.querySelector('button');
-const lista = document.getElementById('ListaGeneral')
+const input = document.querySelector("#ingresarTarea");
+const botonCreaTarea = document.querySelector("#creaTarea");
+const listaTareas = document.querySelector("#listaTareas");
+const listado = document.querySelector("#listado");
 
-function agregarTarea(){
+botonCreaTarea.addEventListener("click", creaTarea);
+
+listaTareas.addEventListener("click", listarTareas);
+/* listaTareas.addEventListener('contextmenu',( )=>{
+    if(listado.style.display!='none'){
+
+    }
+})
+ */
+function listarTareas() {
+  let taskList = JSON.parse(localStorage.getItem("taskList")) || [];
+  listado.innerHTML = generaListadoTareas(taskList);
+  if(listado.style.display==='block'){
+    listado.style.display='none';
+  }else{
+    listado.style.display='block';
+  }
+}
+
+function generaListadoTareas(notas) {
+  let html = `
+    <table style="border-collapse: collapse; border:solid 1px; padding: 5px;  margin:auto; margin-top:5px;">
+        <tbody>
+    `;
+    notas.forEach((nota) => {
+        const note = nota.task || "No se han generado tareas";
+        html += `
+            <tr>
+                <td style="border-collapse: collapse; border:solid 1px; padding: 5px;">${note} 
+                <button id="editarTarea"><i class="bi bi-pencil-square"></i></button>
+                <button id="eliminarTarea"><i class="bi-trash3-fill"></i></button>
+                </td>
+            </tr>
+        `;
+    });
+    html += `
+            </tbody>
+        </table>
+    `;
+    return html;
+}
+
+function creaTarea() {
+  const task = {
+    task: input.value.trim()
+  };
+  if (!task.task) {
+    alert("Por favor ingrese una descripción para la tarea");
+    return;
+  }
+  console.log("tarea a agregar", JSON.stringify(task));
+  let taskList = JSON.parse(localStorage.getItem("taskList")) || [];
+  taskList.push(task);
+  localStorage.setItem("taskList", JSON.stringify(taskList));
+  alert("Tarea registrada con éxito");
+  window.location.reload();
+}
+
+/* function agregarTarea(){
     if(input.value){
         //TAREA NUEVA
         let tareaNueva=document.createElement('div');
@@ -26,5 +85,4 @@ function agregarTarea(){
         //AGREGAR TAREA
         lista.appendChild(tareaNueva);
     }
-}
-boton.addEventListener('click', agregarTarea);
+} */
